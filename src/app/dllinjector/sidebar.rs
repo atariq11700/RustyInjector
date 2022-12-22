ba8364d47080ef3b63fc9fa9250e698ec8dfa4e8
Adam A <31938234+atariq11700@users.noreply.github.com>
2022-12-21 23:19:05 -0700

    added inejection type dropdown

--- src/app/dllinjector/sidebar.rs
+++ src/app/dllinjector/sidebar.rs
@@ -1,4 +1,4 @@
-use egui::{SidePanel, Frame, Color32, Context};
+use egui::{SidePanel, Frame, Color32, Context, ComboBox, Widget, WidgetText};
 
 
 
@@ -8,20 +8,53 @@ pub struct Sidebar {
 }
 
 struct State {
+    injection_type: InjectionTypes
+}
 
+#[derive(PartialEq, Eq, PartialOrd, Ord)]
+enum InjectionTypes {
+    Native,
+    ManualMap,
+}
+
+impl InjectionTypes {
+    fn to_string(&self) -> &str {
+        match self {
+            InjectionTypes::Native => "Native",
+            InjectionTypes::ManualMap => "Manual Map",
+        }
+    }
 }
 
 impl Sidebar {
     pub fn new() -> Sidebar {
         return Sidebar { 
-            state: State {}
+            state: State { 
+                injection_type: InjectionTypes::Native
+            }
         }
     }
-    pub fn show(&self, ctx: &egui::Context) -> () {
+    pub fn show(&mut self, ctx: &egui::Context) -> () {
         SidePanel::left("Left SidePanel")
-            .frame(Frame::default().fill(Color32::LIGHT_BLUE))
+            .frame(Frame::default()
+                .fill(Color32::LIGHT_BLUE))
             .show(ctx, |ui| {
-                ui.label("sidebar")
+                ui.label("sidebar");
+                ComboBox::from_label("Select Inection Type")
+                    .selected_text(&*self.state.injection_type.to_string())
+                    .show_ui(ui, |ui| {
+                        ui.selectable_value(
+                            &mut self.state.injection_type, 
+                            InjectionTypes::Native, 
+                            InjectionTypes::Native.to_string()
+                        );
+                        ui.selectable_value(
+                            &mut self.state.injection_type, 
+                            InjectionTypes::ManualMap, 
+                            InjectionTypes::ManualMap.to_string()
+                        )
+                    })
             });
+        
     }
 }
