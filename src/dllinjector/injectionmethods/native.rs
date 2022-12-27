2bf269e6f67bebc63c16b580e1816ad5208ca280
Adam A <31938234+atariq11700@users.noreply.github.com>
2022-12-27 02:11:50 -0700

    changed function to wrap around unsafe instead of being unsafe

--- src/dllinjector/injectionmethods/native.rs
+++ src/dllinjector/injectionmethods/native.rs
@@ -10,18 +10,20 @@ use winapi::{
 
 use crate::dllinjector::utils;
 
-pub unsafe fn inject(proc: PROCESSENTRY32, dll_path: String) -> bool {
+pub fn inject(proc: PROCESSENTRY32, dll_path: String) -> bool {
     let dll_data = utils::files::is_valid_dll(dll_path);
     if !dll_data.len() > 0 {
         println!("Unable to read dll");
         return false;
     }
 
-    let hTargetProc: HANDLE = OpenProcess(PROCESS_ALL_ACCESS, FALSE as i32, proc.th32ModuleID);
-
-    if (hTargetProc == INVALID_HANDLE_VALUE) {
-        println!("Unable to open target process");
-        return false;
+    unsafe {
+        let target_proc: HANDLE = OpenProcess(PROCESS_ALL_ACCESS, FALSE as i32, proc.th32ModuleID);
+        
+        if (target_proc == INVALID_HANDLE_VALUE) {
+            println!("Unable to open target process");
+            return false;
+        }
     }
 
     return false;
