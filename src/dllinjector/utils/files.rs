cd9e05a4c741a5394013481d63c6a02f7cb7f26c
Adam A <31938234+atariq11700@users.noreply.github.com>
2022-12-27 02:11:13 -0700

    renamed variable for readability

--- src/dllinjector/utils/files.rs
+++ src/dllinjector/utils/files.rs
@@ -7,13 +7,15 @@ use winapi::um::winnt::{
 
 pub fn is_valid_dll(dll_path: String) -> Vec<u8> {
     let file_name = &dll_path;
+    let empty_vec: Vec<u8> = Vec::new();
+
     println!("Checking that {file_name} exists");
 
     let file_res = fs::File::open(&dll_path);
 
     if file_res.is_err() {
         println!("Unable to open/access {file_name}");
-        return Vec::new();
+        return empty_vec;
     }
 
     let mut file = file_res.unwrap();
@@ -21,31 +23,31 @@ pub fn is_valid_dll(dll_path: String) -> Vec<u8> {
 
     if file_metadata_res.is_err() {
         println!("Unable to access {file_name} metadata");
-        return Vec::new();
+        return empty_vec;
     }
 
     let file_metadata = file_metadata_res.unwrap();
     if file_metadata.len() < size_of::<IMAGE_DOS_HEADER>().try_into().unwrap() {
         println!("{file_name} has an invalid size");
-        return Vec::new();
+        return empty_vec;
     }
 
     let mut file_contents: Vec<u8> = Vec::new();
     if file.read_to_end(&mut file_contents).is_err() {
         println!("Unable to read dll");
-        return Vec::new();
+        return empty_vec;
     }
 
     let dos_header: IMAGE_DOS_HEADER =
         unsafe { *(file_contents.as_ptr() as *const IMAGE_DOS_HEADER) };
     if dos_header.e_magic != IMAGE_DOS_SIGNATURE {
         println!("{file_name} is not a valid pe image");
-        return Vec::new();
+        return empty_vec;
     }
 
     if file_metadata.len() < dos_header.e_lfanew.try_into().unwrap() {
         println!("{file_name} has an invalid size");
-        return Vec::new();
+        return empty_vec;
     }
 
     let nt_header = unsafe {
@@ -59,13 +61,13 @@ pub fn is_valid_dll(dll_path: String) -> Vec<u8> {
     #[cfg(target_pointer_width = "64")]
     if file_header.Machine != IMAGE_FILE_MACHINE_AMD64 {
         println!("Host is 64bit and dll is not");
-        return Vec::new();
+        return empty_vec;
     }
 
     #[cfg(target_pointer_width = "32")]
     if file_header.Machine != IMAGE_FILE_MACHINE_I386 {
         println!("Host is 32bit and dll is not");
-        return Vec::new();
+        return null;
     }
 
     println!("Dll is valid");
