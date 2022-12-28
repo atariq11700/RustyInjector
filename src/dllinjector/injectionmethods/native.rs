e6ff3b6a9f80e2be48145525697c7269b3b868f7
Adam A <31938234+atariq11700@users.noreply.github.com>
2022-12-28 06:09:54 -0700

    finished native injection

--- src/dllinjector/injectionmethods/native.rs
+++ src/dllinjector/injectionmethods/native.rs
@@ -1,30 +1,92 @@
 use winapi::{
-    shared::ntdef::FALSE,
+    ctypes::c_void,
+    shared::{
+        basetsd::SIZE_T,
+        minwindef::{FALSE, LPDWORD, LPVOID},
+        ntdef::LPCSTR,
+    },
     um::{
-        handleapi::INVALID_HANDLE_VALUE,
-        processthreadsapi::OpenProcess,
+        handleapi::{CloseHandle, INVALID_HANDLE_VALUE},
+        libloaderapi::{GetModuleHandleA, GetProcAddress},
+        memoryapi::{VirtualAllocEx, WriteProcessMemory},
+        minwinbase::{LPSECURITY_ATTRIBUTES},
+        processthreadsapi::{CreateRemoteThreadEx, OpenProcess, LPPROC_THREAD_ATTRIBUTE_LIST},
         tlhelp32::PROCESSENTRY32,
-        winnt::{HANDLE, PROCESS_ALL_ACCESS},
+        winnt::{HANDLE, MEM_COMMIT, MEM_RESERVE, PAGE_EXECUTE_READWRITE, PROCESS_ALL_ACCESS},
     },
 };
 
 use crate::dllinjector::utils;
 
 pub fn inject(proc: PROCESSENTRY32, dll_path: String) -> bool {
-    let dll_data = utils::files::is_valid_dll(dll_path);
-    if !dll_data.len() > 0 {
+    let dll_data = utils::files::is_valid_dll(dll_path.clone());
+    if !(dll_data.len() > 0) {
         println!("Unable to read dll");
         return false;
     }
 
-    unsafe {
-        let target_proc: HANDLE = OpenProcess(PROCESS_ALL_ACCESS, FALSE as i32, proc.th32ModuleID);
-        
-        if (target_proc == INVALID_HANDLE_VALUE) {
-            println!("Unable to open target process");
-            return false;
-        }
+    let target_proc: HANDLE = unsafe { OpenProcess(PROCESS_ALL_ACCESS, FALSE, proc.th32ProcessID) };
+
+    if target_proc == INVALID_HANDLE_VALUE {
+        println!("Unable to open target process");
+        return false;
     }
 
-    return false;
+    let addr: LPVOID = unsafe {
+        VirtualAllocEx(
+            target_proc,
+            0 as LPVOID,
+            (&dll_path).len(),
+            MEM_RESERVE | MEM_COMMIT,
+            PAGE_EXECUTE_READWRITE,
+        )
+    };
+
+    if (addr as usize) == 0 {
+        println!("Unable to allocte memory inside target process");
+        unsafe { CloseHandle(target_proc) };
+    }
+
+    let mut _f: SIZE_T = 0;
+    if unsafe {
+        WriteProcessMemory(
+            target_proc,
+            addr,
+            dll_path.as_ptr() as *const c_void,
+            dll_path.len(),
+            &mut _f as *mut SIZE_T,
+        )
+    } == 0
+    {
+        println!("Unable to write dll path to target process");
+        unsafe { CloseHandle(target_proc) };
+    }
+
+    let new_thread = unsafe {
+        CreateRemoteThreadEx(
+            target_proc,
+            0 as LPSECURITY_ATTRIBUTES,
+            0,
+            std::mem::transmute(GetProcAddress(
+                GetModuleHandleA("kernel32.dll\0".as_ptr() as LPCSTR),
+                "LoadLibraryA\0".as_ptr() as LPCSTR,
+            )),
+            addr,
+            0,
+            0 as LPPROC_THREAD_ATTRIBUTE_LIST,
+            0 as LPDWORD,
+        )
+    };
+
+    if new_thread == INVALID_HANDLE_VALUE {
+        println!("Unable to create a remote thread");
+        unsafe { CloseHandle(target_proc) };
+    }
+
+    unsafe {
+        CloseHandle(new_thread);
+        CloseHandle(target_proc);
+    };
+
+    return true;
 }
