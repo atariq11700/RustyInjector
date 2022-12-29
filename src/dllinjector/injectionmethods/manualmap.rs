3e00cd2127758c2742ebc786f550c93e7a589042
Adam A <31938234+atariq11700@users.noreply.github.com>
2022-12-28 22:49:38 -0700

    continue working on manualmap injection

--- src/dllinjector/injectionmethods/manualmap.rs
+++ src/dllinjector/injectionmethods/manualmap.rs
@@ -1,16 +1,21 @@
+use std::ptr::addr_of;
+
 use winapi::{
+    ctypes::c_void,
     shared::{
-        minwindef::{BOOL, DWORD, FARPROC, HINSTANCE, HMODULE, LPVOID, WORD},
-        ntdef::{LPCSTR, HANDLE}, basetsd::SIZE_T,
+        basetsd::SIZE_T,
+        minwindef::{BOOL, DWORD, FARPROC, HINSTANCE, HMODULE, LPCVOID, LPVOID, WORD},
+        ntdef::{HANDLE, LPCSTR},
     },
     um::{
-        handleapi::{INVALID_HANDLE_VALUE, CloseHandle},
-        memoryapi::VirtualAllocEx,
+        handleapi::{CloseHandle, INVALID_HANDLE_VALUE},
+        memoryapi::{VirtualAllocEx, VirtualFreeEx, WriteProcessMemory},
         processthreadsapi::OpenProcess,
         tlhelp32::PROCESSENTRY32,
         winnt::{
             IMAGE_DOS_HEADER, IMAGE_NT_HEADERS, IMAGE_REL_BASED_DIR64, IMAGE_REL_BASED_HIGHLOW,
-            MEM_COMMIT, MEM_RESERVE, PAGE_EXECUTE_READWRITE, PROCESS_ALL_ACCESS, IMAGE_SECTION_HEADER
+            IMAGE_SECTION_HEADER, MEM_COMMIT, MEM_FREE, MEM_RESERVE, PAGE_EXECUTE_READWRITE,
+            PIMAGE_NT_HEADERS, PIMAGE_SECTION_HEADER, PROCESS_ALL_ACCESS,
         },
     },
 };
@@ -39,8 +44,17 @@ fn needs_reloc(reloc_info: WORD) -> bool {
     return (reloc_info >> 0x0C) == IMAGE_REL_BASED_HIGHLOW;
 }
 
-fn image_first_section(nt_header: IMAGE_NT_HEADERS) -> IMAGE_SECTION_HEADER {
-    
+//todo: broken
+fn image_first_section(pnt_header: PIMAGE_NT_HEADERS) -> PIMAGE_SECTION_HEADER {
+    return unsafe {
+        pnt_header
+            .add(memoffset::offset_of!(IMAGE_NT_HEADERS, OptionalHeader))
+            .add(
+                (*pnt_header as IMAGE_NT_HEADERS)
+                    .FileHeader
+                    .SizeOfOptionalHeader as usize,
+            ) as PIMAGE_SECTION_HEADER
+    };
 }
 
 pub fn inject(proc: PROCESSENTRY32, dll_path: String) -> bool {
@@ -50,7 +64,8 @@ pub fn inject(proc: PROCESSENTRY32, dll_path: String) -> bool {
         return false;
     }
 
-    let target_proc: HANDLE = unsafe { OpenProcess(PROCESS_ALL_ACCESS, false as BOOL, proc.th32ProcessID) };
+    let target_proc: HANDLE =
+        unsafe { OpenProcess(PROCESS_ALL_ACCESS, false as BOOL, proc.th32ProcessID) };
 
     if target_proc == INVALID_HANDLE_VALUE {
         println!("Unable to open target process");
@@ -77,22 +92,61 @@ pub fn inject(proc: PROCESSENTRY32, dll_path: String) -> bool {
     };
 
     if base_addr_ex as usize == 0 {
-        base_addr_ex =  unsafe {VirtualAllocEx(
-            target_proc,
-            0 as LPVOID,
-            optional_header.SizeOfImage as SIZE_T,
-            MEM_RESERVE | MEM_COMMIT,
-            PAGE_EXECUTE_READWRITE,
-        )};
+        base_addr_ex = unsafe {
+            VirtualAllocEx(
+                target_proc,
+                0 as LPVOID,
+                optional_header.SizeOfImage as SIZE_T,
+                MEM_RESERVE | MEM_COMMIT,
+                PAGE_EXECUTE_READWRITE,
+            )
+        };
     }
 
     if base_addr_ex as usize == 0 {
         println!("Unable to allocate memory inside target process for dll");
-        unsafe { CloseHandle(target_proc)};
+        unsafe { CloseHandle(target_proc) };
     }
 
-    let section_header = image_first_section(nt_header);
-    
+    let mut section_header =
+        image_first_section(&nt_header as *const IMAGE_NT_HEADERS as PIMAGE_NT_HEADERS);
+
+    for i in 0..file_header.NumberOfSections {
+        unsafe {
+            if (*section_header).SizeOfRawData > 0 {
+                let name = (*section_header).Name;
+                if WriteProcessMemory(
+                    target_proc,
+                    base_addr_ex.add((*section_header).VirtualAddress as usize),
+                    dll_data
+                        .as_ptr()
+                        .add((*section_header).PointerToRawData as usize)
+                        as LPCVOID,
+                    (*section_header).SizeOfRawData as SIZE_T,
+                    0 as *mut usize,
+                ) == 0
+                {
+                    println!("Unable to map section {name:?} into target process memory");
+                    CloseHandle(target_proc);
+                    VirtualFreeEx(
+                        target_proc,
+                        base_addr_ex,
+                        optional_header.SizeOfImage as SIZE_T,
+                        MEM_FREE,
+                    );
+                    return false;
+                }
+                println!(
+                    "Mapped dll section {:?} {} into target process as 0x{:x}",
+                    name,
+                    (*section_header).SizeOfRawData,
+                    base_addr_ex.add((*section_header).VirtualAddress as usize) as usize
+                );
+            }
+            section_header =
+                section_header.add(std::mem::size_of::<IMAGE_SECTION_HEADER>() as usize);
+        }
+    }
 
     return false;
 }
