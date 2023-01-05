4edc1c3d8f57fe665c2d389e5c3da29372aa8eda
Adam Alder <atariq11700@gmail.com>
2023-01-04 20:03:53 -0700

    dont build with icons in debug mode so debuggers dont have to set cwd

--- build.rs
+++ build.rs
@@ -1,6 +1,7 @@
 extern crate winres;
 
 fn main() {
+    #[cfg(not(debug_assertions))]
     if cfg!(target_os = "windows") {
         let mut res = winres::WindowsResource::new();
         res.set_icon("res/icon.ico");
--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,14 @@ mod dllinjector;
 mod utils;
 
 fn main() {
+    #[cfg(not(debug_assertions))]
     let options = eframe::NativeOptions {
         icon_data: Some(utils::files::load_icon("res/icon.png")),
         ..Default::default()
     };
+
+    #[cfg(debug_assertions)]
+    let options = eframe::NativeOptions::default();
 
     eframe::run_native(
         "Rusty Injector",
